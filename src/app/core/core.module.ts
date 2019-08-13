eae74149033b022bf06f427cebf03e3fa12874e0
Nicolas Fazio <contact@nicolasfazio.ch>
2019-08-13 12:01:06 +0200

    upd: use tokeninterceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { UpdatesNotificationComponent } from './components/updates-notification/updates-notification.component';
 import { IonicModule } from '@ionic/angular';
 import { DemoModalComponent } from './components/demo-modal/demo-modal.component';
+import { TokenInterceptor } from './services/token-interceptor.service';
 
 const COMPONENTS = [
   UpdatesNotificationComponent,
@@ -24,6 +25,13 @@ const ENTRY_COMPONENTS = [
     HttpClientModule,
     CommonModule,
     IonicModule
+  ],
+  providers: [
+     {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    },
   ],
   exports: [
     ...COMPONENTS,
